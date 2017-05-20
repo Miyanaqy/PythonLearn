50dfbf01f885c6a7d0a749b9eb2fcafa5b1dc29c
Miyanaqy <2247762766@qq.com>
2017-05-20 16:56:53 +0800

    upload

--- math/circular_move.py
+++ math/circular_move.py
@@ -2,13 +2,18 @@ import pygame, sys, math
 
 pygame.init()
 screen = pygame.display.set_mode([640,640])
-angle = 0
+angle =90
+print angle
+juli = 1
+
+x,y = 0,0
 screen.fill([255,255,255])
 while True:
     pygame.time.delay(10)
-    angle += 1
-    x = 200 * math.sin(angle * math.pi / 180) + 320
-    y = 200 * math.cos(angle * math.pi / 180) + 320
+    screen.fill([255,255,255])
+    x = juli * math.sin(angle * math.pi / 180) + x
+    y = juli * math.cos(angle * math.pi / 180) *10
+    print x ,y
     pygame.draw.rect(screen,[0,0,0],[x,y,5,5],0)
     pygame.display.flip()
     for event in pygame.event.get():
